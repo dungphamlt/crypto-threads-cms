02b5e9d21c81f447ec76555e313c5886adacbc9f
ManhVu-07 <manhvd1507@gmail.com>
2025-11-13 14:40:13 +0700

    feat: add quick edit

--- src/components/post/index.tsx
+++ src/components/post/index.tsx
@@ -3,3 +3,4 @@ export { default as PostGridLayout } from "./post-grid-layout";
 export { default as PostTableLayout } from "./post-table-layout";
 export { default as PostCreate } from "./post-create";
 export { default as PostViewDetail } from "./post-view-detail";
+export { default as PostQuickEdit } from "./post-quick-edit";
